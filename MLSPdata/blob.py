1d50782f3aa305c72e756e52f6969850568d8af1
grizztastic <alan.grizzaffi@sv.cmu.edu>
2019-11-20 11:31:33 -0800

    blob.py

--- MLSPdata/blob.py
+++ MLSPdata/blob.py
@@ -16,7 +16,7 @@ def numericalSort(value):
 np.set_printoptions(threshold=sys.maxsize)
 
 #read in files from folder and create a path to open the folder; place them into a list to access later
-folders = glob.glob('frames/RHlEdXq2DuI_7')
+folders = glob.glob('frames/Usable_frames')
 
 frames_list = []
 for folder in folders:
@@ -34,33 +34,25 @@ for frame in frames_list:
     data[:250000] = 0
     data[650000:] = 0
     gs_image_list.append(data)
-    #gs_image_2 = gs_image_list[i]
 
 frame_subtract_list = [gs_image_list[i] - gs_image_list[i-1] for i in range(len(gs_image_list)) if i > 0]
-#frame_subtract_list = [0 for frame in frame_subtract_list for value in frame if value < 40]
-'''
+#frame_subtract_list = [0 for frame in frame_subtract_list for value in frame if value < 30]
+#frame_subtract_list = [255 for frame in frame_subtract_list for value in frame if value > 30]
+
+
 for frame in frame_subtract_list:
-    frame[:250000] = 0
-    frame[600000:] = 0
-    for j in range(len(frame)):git 
+    for j in range(len(frame)):
         if frame[j] < 30:
             frame[j] = 0
-'''
+        else:
+            frame[j] = 255
+
 frame_subtract_list_reshape = [frame_subtract_list[j].reshape((720, 1280)) for j in range(len(frame_subtract_list))]
-frame_subtract_list_reshape[120][frame_subtract_list_reshape[120] < 25] = 0
-frame_subtract_list_reshape[120][frame_subtract_list_reshape[120] > 25] = 255
-image_no_noise = Image.fromarray(frame_subtract_list_reshape[120].astype('uint8'))  # turn into an image
-image_no_noise.show()
-#    if i > 0:
- #       frame_subtract = gs_image_list[i] - gs_image_list[i-1]
-  #      frame_subtract[:300000] = 0
-  #      frame_subtract[600000:] = 0
-  #      for j in range(len(frame_subtract)):
-  #          if frame_subtract[j] < 30:
-  #              gs_image_2[j] = 0
-    #frame_subtract[frame_subtract < 40] = 0
-    #frame_subtract[frame_subtract > 200] = 0
-    #print(frame_subtract)
-       # image_reshape = gs_image_2.reshape((720, 1280))
-        #image_no_noise = Image.fromarray(image_reshape.astype('uint8'))  # turn into an image
-        #image_no_noise.show()
+
+ct = 1
+for blob_image in frame_subtract_list_reshape:
+    #blob_image[240:480,:420] = 0
+    image_no_noise = Image.fromarray(blob_image.astype('uint8'))  # turn into an image
+    image_no_noise.save('frames/Blob_frames/blob_image_' + str(ct) + '.jpg')
+    ct += 1
+    #image_no_noise.show()
